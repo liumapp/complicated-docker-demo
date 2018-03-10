5b61acf7cf47e51886df0fabdbe9fc6e0f51c0df
liumapp <liumapp.com@gmail.com>
2018-03-10 15:33:54 +0800

    eureka server a  & b done

--- docker-multy-eureka/server-b/src/main/java/com/liumapp/demo/docker/multy/eureka/server/b/MultyEurekaB.java
+++ docker-multy-eureka/server-b/src/main/java/com/liumapp/demo/docker/multy/eureka/server/b/MultyEurekaB.java
@@ -2,17 +2,19 @@ package com.liumapp.demo.docker.multy.eureka.server.b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/10/18 3:33 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
-public class Main {
+@EnableEurekaServer
+@SpringBootApplication
+public class MultyEurekaB {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(MultyEurekaB.class , args);
     }
 
 }
