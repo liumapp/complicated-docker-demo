1924a9028de14c0676df47c34f246afac791df2e
liumapp <liumapp.com@gmail.com>
2018-03-12 14:24:27 +0800

    config eureka server

--- docker-multy-eureka/server-profile/src/main/java/com/liumapp/demo/docker/multy/eureka/server/profile/EurekaProfile.java
+++ docker-multy-eureka/server-profile/src/main/java/com/liumapp/demo/docker/multy/eureka/server/profile/EurekaProfile.java
@@ -2,17 +2,19 @@ package com.liumapp.demo.docker.multy.eureka.server.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/12/18 2:23 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
-public class Main {
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaProfile {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(EurekaProfile.class , args);
     }
 
 }
