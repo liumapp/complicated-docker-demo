6f06365d2e0f2b575c943021bd4761a1a0b6f6bf
liumapp <liumapp.com@gmail.com>
2018-03-12 14:41:24 +0800

    conf server-maven done

--- docker-multy-eureka/server-maven/src/main/java/com/liumapp/demo/docker/multy/eureka/server/maven/ServerMaven.java
+++ docker-multy-eureka/server-maven/src/main/java/com/liumapp/demo/docker/multy/eureka/server/maven/ServerMaven.java
@@ -2,17 +2,19 @@ package com.liumapp.demo.docker.multy.eureka.server.maven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/12/18 2:40 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
-public class Main {
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerMaven {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(ServerMaven.class , args);
     }
 
 }
